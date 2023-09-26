cec8110a5e691d447e1c38931b8f0f6b80537323
Aditya Raj <101439988+RajAditya01@users.noreply.github.com>
2023-09-26 18:11:40 +0530

    .

--- DB/conn.js
+++ DB/conn.js
@@ -1,13 +1,11 @@
 const mongoose =require('mongoose');
 const DB = process.env.DATABASE;
-
+console.log(DB);
 mongoose.connect(DB, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false
 }).then(() => {
     console.log(`Connection successful`);
 }).catch((err) => {
     console.log(`No connection`);
+    console.log(err);
 });
+
